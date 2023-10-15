3da8c81e131861465ddd60ae3c3aaaa3424b5395
Jonathan Cole <JonathanPeterCole@gmail.com>
2023-10-15 21:32:07 +0100

    Check intent action and category to determine if the app will be killed on Android 12+

--- android/src/main/kotlin/dev/jonathancole/system_back_confirmation/SystemBackConfirmationPlugin.kt
+++ android/src/main/kotlin/dev/jonathancole/system_back_confirmation/SystemBackConfirmationPlugin.kt
@@ -1,32 +1,56 @@
 package dev.jonathancole.system_back_confirmation
 
-import androidx.annotation.NonNull
+import android.app.Activity
+import android.content.Intent
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** SystemBackConfirmationPlugin */
-class SystemBackConfirmationPlugin: FlutterPlugin, MethodCallHandler {
+class SystemBackConfirmationPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel : MethodChannel
+  private var activity : Activity? = null
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "system_back_confirmation")
     channel.setMethodCallHandler(this)
   }
 
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getBackWillKillApp") {
-      if (android.os.Build.VERSION.SDK_INT < 31) {
-        result.success(true)
+      activity?.intent?.hasCategory(Intent.CATEGORY_LAUNCHER)
+      if (android.os.Build.VERSION.SDK_INT >= 31) {
+        val intent = activity?.intent
+        val willKillApp = intent != null &&
+            (intent.action != Intent.ACTION_MAIN || !intent.hasCategory(Intent.CATEGORY_LAUNCHER))
+        result.success(willKillApp)
       } else {
-        result.success(false)
+        result.success(true)
       }
     } else {
       result.notImplemented()
     }
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
